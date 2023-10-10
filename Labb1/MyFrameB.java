1b621635feba0c71248e5a05f65bc3653a6b5f9f
lulleliu <lucasliu@live.se>
2023-10-10 12:57:23 +0200

    DEL B Uppdaterad

--- Labb1/MyFrameB.java
+++ Labb1/MyFrameB.java
@@ -1,19 +1,28 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class MyFrameB extends JFrame{
 
     public MyFrameB(String name){
         // 2. Construct the frame object according to its super class (JFrame).
         super(name);
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
 
         // 3. Creates a new button object and adds it. GO TO "MyButton.java" for step 4.
         MyButtonB button = new MyButtonB("toggle");
-        this.add(button);
-        // 5. The button is added to the frame and an actionListener is added to the button
+        MyButtonB button2 = new MyButtonB("toggle");
 
+        panel.add(button);
+        panel.add(button2);
+
+        this.add(panel);
+        // 5. The button is added to the frame and an actionListener is added to the button
     }
 
     public static void main(String[] args){
